5c4b2314d8d67dad2b062c525ed7b9bc74e61b4c
danielsan80 <danilo.sanchi@gmail.com>
2013-06-27 15:17:01 +0200

    disable host command

--- php/endishost.php
+++ php/endishost.php
@@ -3,6 +3,21 @@ copy('/etc/hosts', __DIR__.'/../backup/hosts/hosts_'.date('Y-m-d_H-i-s'));
 
 $hosts = file_get_contents('/etc/hosts');
 
+$mode = 'enable';
+if (isset($argv[2])) {
+    switch($argv[2]) {
+        case'enable':
+            $mode = 'enable';
+            break;
+        case'disable':
+            $mode = 'disable';
+            break;
+        defalut:
+            echo 'The second arg must be enable or disable'."\n";
+            die();
+    }
+}
+
 if (!isset($argv[1])) {
     echo 'Specify the block name, please'."\n";
     die();
@@ -12,9 +27,19 @@ $name= $argv[1];
 
 $pattern = '/(#[ ]?block '.$name.')(?P<content>[.\s\w#]*)(#[ ]?endblock)/';
 
-preg_match($pattern, $hosts, $matches);
 
-$content = preg_replace('/\n#/', "\n", $matches['content']);
+preg_match($pattern, $hosts, $matches);
+if (!isset($matches['content'])) {
+    echo 'The block "'.$name.'" doesn\'t exist\n"';
+    die();
+}
+
+    $content = preg_replace('/\n#/', "\n", $matches['content']);
+if ($mode=='disable') {
+    $content = preg_replace('/\n/', "\n#", $content);
+    $content = preg_replace('/#$/', "", $content);
+}
+
 $hosts = preg_replace($pattern, '${1}'.$content.'${3}', $hosts);
 
 file_put_contents('/etc/hosts', $hosts);
